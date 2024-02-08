1df84585a9d2d62dfa29be3e2be314702e0dac1d
Philipp Sauter <philipp.sauter@exoscale.ch>
2024-02-08 18:14:02 +0100

    fix panic

--- internal/integ/integ_test.go
+++ internal/integ/integ_test.go
@@ -235,7 +235,11 @@ func TestSnapshot(t *testing.T) {
 
 	awaitExpectation(t, "Bound", func() interface{} {
 		pvc, err := pvcClient.Get(ns.CTX, "my-snap-1-pvc", metav1.GetOptions{})
-		assert.NoError(t, err)
+
+		if err != nil {
+			assert.NoError(t, err)
+			return "error"
+		}
 
 		return pvc.Status.Phase
 	})
@@ -246,7 +250,10 @@ func TestSnapshot(t *testing.T) {
 
 	awaitExpectation(t, 0, func() interface{} {
 		snapshots, err := snapshotClient.List(ns.CTX, v1.ListOptions{})
-		assert.NoError(t, err)
+		if err != nil {
+			assert.NoError(t, err)
+			return 0
+		}
 
 		return len(snapshots.Items)
 	})
