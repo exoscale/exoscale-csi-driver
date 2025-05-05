a0e8266a252f01ab2dbe8ed4c74f48ccff19ffb1
miledxz <zedsprogramms@gmail.com>
2025-05-05 21:03:37 +0200

    Unit tests: update helper driver tests

--- driver/helpers_test.go
+++ driver/helpers_test.go
@@ -1,6 +1,8 @@
 package driver
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
@@ -109,5 +111,62 @@ func TestGetNewVolumeSize(t *testing.T) {
 		res, err := getNewVolumeSize(test.capRange)
 		require.Equal(t, test.err, err)
 		require.Equal(t, test.res, res)
+	}
+}
+
+func TestCreateMountPoint(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	testBench := []struct {
+		name        string
+		relPath     string
+		file        bool
+		preCreate   bool
+		expectError bool
+		isDir       bool
+	}{
+		{
+			name:    "create directory mount point",
+			relPath: "parent/dir",
+			file:    false,
+			isDir:   true,
+		},
+		{
+			name:    "create file mount point",
+			relPath: "parent/file.txt",
+			file:    true,
+			isDir:   false,
+		},
+		{
+			name:      "existing path no error",
+			relPath:   "parent",
+			file:      false,
+			preCreate: true,
+			isDir:     true,
+		},
+	}
+
+	for _, test := range testBench {
+		t.Run(test.name, func(t *testing.T) {
+			fullPath := filepath.Join(tmpDir, test.relPath)
+
+			if test.preCreate {
+				err := os.MkdirAll(fullPath, 0755)
+				require.NoError(t, err)
+			}
+
+			err := createMountPoint(fullPath, test.file)
+
+			if test.expectError {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+
+			info, err := os.Stat(fullPath)
+			require.NoError(t, err)
+			require.Equal(t, test.isDir, info.IsDir())
+		})
 	}
 }
